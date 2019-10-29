df31a52d2e0a9cdd84cb711cbdede8ba2f90c9f8
ethand9 <43592552+ethand9@users.noreply.github.com>
2019-10-28 20:00:13 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "location.h"
+using namespace std;
+
+
+int main()
+{
+    location maze; //object of location
+
+    maze.createMaze();
+    maze.currentLocation();
+    return 0;
+}
+
+
